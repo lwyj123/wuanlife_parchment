d4b7a8eee920c1bfdcc09357c56f6445de1f3cb2
liangyuyi <liangyuyi@bytedance.com>
2017-12-28 10:51:44 +0800

    initial using quill-parchment

---
+++ webpack.conf.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: ['./src/parchment.ts'],
+  output: {
+    filename: 'parchment.js',
+    library: 'Parchment',
+    libraryTarget: 'umd',
+    path: __dirname + '/dist',
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
+  module: {
+    rules: [{ test: /\.ts$/, use: 'ts-loader' }],
+  },
+  devtool: 'source-map',
+};
